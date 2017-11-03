585eb1ad7c8f7a853a6ebf9e659ebf5d6e9fb23e
Di Chen <dchen20@ncsu.edu>
2017-11-03 01:04:32 -0400

    Add state of the art classifiers

--- fft.py
+++ fft.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.mixture import GaussianMixture
+from sklearn.svm import LinearSVC
 import collections
 import os
+
+from ABCD import ABCD
 
 PRE, REC, SPEC, ACC, F1 = 5, 4, 3, 2, 1
 # training / testing split
@@ -23,6 +29,7 @@ class FFT(object):
         self.performance = [collections.defaultdict(dict) for _ in xrange(cnt)]
         self.selected = [{} for _ in range(cnt)]
         self.tree_info = [collections.defaultdict(dict) for _ in xrange(cnt)]
+        self.soa = None
 
 
     "Get all possible tree structure"
@@ -58,18 +65,8 @@ class FFT(object):
         fp = len(pos.loc[pos[self.target] == 0])
         tn = len(neg.loc[neg[self.target] == 0])
         fn = len(neg.loc[neg[self.target] == 1])
-        pre, rec, spec, acc, f1 = self.get_performance(tp, fp, tn, fn)
+        pre, rec, spec, acc, f1 = get_performance(tp, fp, tn, fn)
         return tp, fp, tn, fn, pre, rec, spec, acc, f1
-
-
-    "Given TP, FP, TN, FN, get all the other metrics. "
-    def get_performance(self, tp, fp, tn, fn):
-        pre = 1.0 * tp / (tp + fp) if (tp + fp) != 0 else 0
-        rec = 1.0 * tp / (tp + fn) if (tp + fn) != 0 else 0
-        spec = 1.0 * tn / (tn + fp) if (tn + fp) != 0 else 0
-        acc = 1.0 * (tp + tn) / (tp + tn + fp + fn) if (tp + tn + fp + fn) != 0 else 0
-        f1 = 2.0 * tp / (2.0 * tp + fp + fn) if (2.0 * tp + fp + fn) != 0 else 0
-        return [round(x, 1) for x in [pre, rec, spec, acc, f1]]
 
 
     "Given tree and level, get the node info"
@@ -116,27 +113,41 @@ class FFT(object):
         for i in range(self.tree_cnt):
             self.describe_tree(i)
             tp, fp, tn, fn, mcu = self.tree_info[i]["general"]
-            metric = self.get_performance(tp, fp, tn, fn)
-            roc[i] = [1 - metric[-SPEC], metric[-REC]]
+            metric = get_performance(tp, fp, tn, fn)
+            roc[i] = [1-metric[-SPEC], metric[-REC]]
             if not selected or metric[goal] > selected[goal]:
                 selected = [i] + metric
             print " \t ".join([str(x) for x in [i, round(mcu,1)] + metric])
         print "-------------------------------------------------------"
         print "The selected tree id is :" + str(selected[0])
+
+        # Get the state of the art classifiers.
+        if not self.soa:
+            self.get_soa()
+
+
         # plot ROC
         fig, ax = plt.subplots()
-        x, y = zip(*roc)
-        ax.scatter(x, y, s=20)
-        ax.scatter(x[selected[0]], y[selected[0]], c='r', s=30)
-        x, y = [0.001 * i for i in range(1000)], [0.001 * i for i in range(1000)]
-        ax.scatter(x, y, s=4, marker=".")
-        for i in range(self.tree_cnt):
-            ax.annotate(i, (roc[i][0], roc[i][1]))
         ax.set_title('ROC')
-        ax.set_xlabel("1 - Specificity (FAR)")
-        ax.set_ylabel("Sensitivity (HR)")
-        ax.set_xlim(0, 1.05)
-        ax.set_ylim(0, 1.05)
+        ax.set_xlabel("False Alarm Rates")
+        ax.set_ylabel("Recall")
+        ax.set_xlim(-0.05, 1.05)
+        ax.set_ylim(-0.05, 1.05)
+        # plot diagonal
+        x, y = [0.001 * i for i in range(1000)], [0.001 * i for i in range(1000)]
+        ax.scatter(x, y, s=4)
+        # plot fft peformances
+        ax.scatter(x[selected[0]], y[selected[0]], c='r', s=100)
+        ax.annotate("B_FFT", (x[selected[0]], y[selected[0]]))
+        for i in range(self.tree_cnt):
+            ax.scatter(roc[i][0], roc[i][1], s=100)
+            ax.annotate(i, (roc[i][0], roc[i][1]))
+        # plot state of the art performance
+        soa_color = ['g', 'c', 'm', 'y']
+        soa_name = ["SL", "NB", "EM", "SMO"]
+        for i in range(4):
+            ax.scatter(1-self.soa[i][-3], self.soa[i][-4], c=soa_color[i], s=100)
+            ax.annotate(soa_name[i], (1-self.soa[i][-3], self.soa[i][-4]))
         plt.show()
         return selected[0]
 
@@ -161,7 +172,7 @@ class FFT(object):
         print "\t".join([x.ljust(6, " ") for x in ["TP", "FP", "TN", "FN"]])
         print "\t".join([str(x).ljust(6, " ") for x in [tp, fp, tn, fn]])
         print "\t".join([str(x).ljust(6, " ") for x in ["MCU", "PRE", "REC", "SPEC", "ACC", "F1"]])
-        print "\t".join([str(x).ljust(6, " ") for x in [round(mcu,1)] + self.get_performance(tp, fp, tn, fn)])
+        print "\t".join([str(x).ljust(6, " ") for x in [round(mcu,1)] + get_performance(tp, fp, tn, fn)])
         print "======================================="
 
 
@@ -200,12 +211,65 @@ class FFT(object):
             self.grow(self.train, i, 0)
 
 
+    "Get performance of state of the art classifiers"
+    def get_soa(self):
+        SL = LogisticRegression()   # simple logistic
+        NB = GaussianNB()           # Naive Bayes\
+        EM = GaussianMixture()      # Expectation Maximization
+        SMO = LinearSVC()           # Support Vector Machines
+        train_data, train_label = self.train.iloc[:, 3:-1], self.train.iloc[:, -1]
+        test_data, test_label = self.test.iloc[:, 3:-1], self.test.iloc[:, -1]
+        m_SL = do_classification(train_data, test_data, train_label, test_label, SL)
+        m_NB = do_classification(train_data, test_data, train_label, test_label, NB)
+        m_EM = do_classification(train_data, test_data, train_label, test_label, EM)
+        m_SMO = do_classification(train_data, test_data, train_label, test_label, SMO)
+        self.soa = [m_SL, m_NB, m_EM, m_SMO]
+
+
+
+"Given TP, FP, TN, FN, get all the other metrics. "
+def get_performance(tp, fp, tn, fn):
+    pre = 1.0 * tp / (tp + fp) if (tp + fp) != 0 else 0
+    rec = 1.0 * tp / (tp + fn) if (tp + fn) != 0 else 0
+    spec = 1.0 * tn / (tn + fp) if (tn + fp) != 0 else 0
+    acc = 1.0 * (tp + tn) / (tp + tn + fp + fn) if (tp + tn + fp + fn) != 0 else 0
+    f1 = 2.0 * tp / (2.0 * tp + fp + fn) if (2.0 * tp + fp + fn) != 0 else 0
+    return [round(x, 1) for x in [pre, rec, spec, acc, f1]]
+
+
+def get_abcd(predict, truth):
+    # pos > 0, neg == 0
+    n = len(predict)
+    tp, fp, tn, fn = 0, 0, 0, 0
+    for i in range(n):
+        if predict[i] > 0 and truth[i] > 0:
+            tp += 1
+        elif predict[i] > 0 and truth[i] == 0:
+            fp += 1
+        elif predict[i] == 0 and truth[i] == 0:
+            tn += 1
+        elif predict[i] == 0 and truth[i] > 0:
+            fn += 1
+    return tp, fp, tn, fn
+
+
+def do_classification(train_data, test_data, train_label, test_label, clf=''):
+    if not clf:
+        clf = LinearSVC()
+    clf.fit(train_data, train_label)
+    prediction = clf.predict(test_data)
+    tp, fp, tn, fn = get_abcd(prediction, np.array(test_label))
+    pre, rec, spec, acc, f1 = get_performance(tp, fp, tn, fn)
+    return tp, fp, tn, fn, pre, rec, spec, acc, f1
+
+
 cwd = os.getcwd()
 csv_path = os.path.join(cwd, "ivy-2.0.csv")
 df = pd.read_csv(csv_path)
 print df.describe()
 
-fft = FFT(df, goal=F1)
+fft = FFT(df, goal=PRE)
+fft.get_soa()
 fft.build_trees()
 fft.plot_tree(show_metrics=True)
 # fft.grow(fft.train)
